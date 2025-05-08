2fb8e3f98727a31efe068b29f7957f79633fd49c
galaxykkim <galaxykkim.dev@gmail.com>
2025-05-08 11:20:25 +0900

    레이아웃 정렬

--- sputnik/app/src/main/java/gkk/app/sputnik/ui/MainActivity.kt
+++ sputnik/app/src/main/java/gkk/app/sputnik/ui/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import gkk.app.sputnik.common.Screen
+import gkk.app.sputnik.common.TextStyles
 import gkk.app.sputnik.ui.screen.home.HomeScreen
 import gkk.app.sputnik.ui.screen.livedata.ExampleLiveDataScreen
 
@@ -23,8 +33,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                MainScreen()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+            ) { innerPadding ->
+                Column(modifier = Modifier.fillMaxSize()
+                    .padding(WindowInsets.statusBars.asPaddingValues())
+                ) {
+                    TopBar()
+                    MainScreen()
+                }
             }
         }
     }
@@ -39,6 +56,20 @@ fun MainScreen() {
         composable(Screen.EXAMPLE_LIVEDATA) { ExampleLiveDataScreen(navController) }
     }
 }
+
+
+@Composable
+fun TopBar() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("Examples", style = TextStyles.Title)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
--- sputnik/app/src/main/java/gkk/app/sputnik/ui/screen/home/HomeScreen.kt
+++ sputnik/app/src/main/java/gkk/app/sputnik/ui/screen/home/HomeScreen.kt
@@ -4,16 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import gkk.app.sputnik.common.TextStyles
 
 @Composable
 fun HomeScreen(
@@ -30,11 +33,11 @@ fun HomeScreen(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text("GKK Lab. Examples")
-        Spacer(modifier = Modifier.height(16.dp))
-
         Button(onClick = {
             viewModel.handleIntent(HomeIntent.NavigateToExampleLiveData)
         }) {
